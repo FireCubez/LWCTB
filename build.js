3789c0dd5e5b6a9aad09713ab5fea1c62bcfec03
firecubez <firecubezyt@gmail.com>
2020-02-05 18:58:03 +0200

    Change build process

---
+++ build.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
+
+for(let f of fs.readdirSync("src")) {
+	console.log("File:", f);
+	if(f.endsWith(".pegjs")) {
+		console.log("Running pegjs...");
+		let r = child_process.spawnSync("npx", ["pegjs", "-o", path.join("build", f.slice(0, -5) + "js"), path.join("src", f)], {
+			stdio: "inherit"
+		});
+		if(r.error) throw r;
+		continue;
+	}
+	console.log("Copying", f);
+	fs.copyFileSync(path.join("src", f), path.join("build", f));
+}
